7367f6e004f270cea930905fabe15674b0dbc133
Drew Keat <thedrewkeat@gmail.com>
2021-07-23 11:40:43 -0600

    done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ActiveRecord::Base
+    # The title cannot be blank
+    validates :title, length: {minimum: 1}
+    # The category is either "Fiction" or "Non-Fiction"
+    validates_inclusion_of :category, in: %w( Fiction Non-Fiction ), on: :create, message: "Category must be Fiction or Non-Fiction"
+    # The content is at least 100 characters long.
+    validates :content, length: {minimum: 100}
+
 end
